00cece19e4ecb12e901f69fbf290e5a5f1be43fc
manishdayama <manish.dayama@5exceptions.com>
2024-06-21 17:36:06 +0530

    sql

--- voicecontrol.sql
+++ voicecontrol.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 11:34 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Generation Time: Jun 21, 2024 at 02:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` int(11) NOT NULL,
+  `adminId` int(11) DEFAULT NULL,
+  `activity` text NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deletedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`id`, `adminId`, `activity`, `createdAt`, `deletedAt`) VALUES
+(1, NULL, 'admin@gmail.com deleted user john', '2024-06-20 15:21:24', NULL),
+(2, NULL, 'smith updated user andrew', '2024-06-20 15:22:05', NULL),
+(3, 1, 'admin modified andrew smith admin', '2024-06-21 09:36:50', NULL),
+(4, 1, 'root admin modified albert istein user', '2024-06-21 10:42:52', NULL),
+(6, 1, 'root admin deleted sowel chempi user', '2024-06-21 11:30:20', NULL),
+(7, 1, 'root admin created allen solly admin', '2024-06-21 11:31:38', NULL),
+(8, 1, 'root admin modified allen solly admin', '2024-06-21 11:32:01', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `admins`
 --
 
@@ -35,15 +62,17 @@ CREATE TABLE `admins` (
   `permissions` text NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
   `entrydt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `permissions`, `status`, `entrydt`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2b$10$DM7X/0CBq8WF6mscHRu/PeWLiuAurhqaQdHdV6t1lgg6/zbFBJ4uq', 'dashboard,admins,users', 1, '2023-11-07 14:08:19'),
-(2, 'aad', 'test@gmail.com', '$2b$10$GvyBOIpUpcy5Ks2aTwa8hey3GJL1oA9hg09K6Oy0s.olYvEGNUFbq', 'dashboard', 1, '2023-11-08 07:16:55');
+(1, 'root admin', 'admin@gmail.com', '$2b$10$DM7X/0CBq8WF6mscHRu/PeWLiuAurhqaQdHdV6t1lgg6/zbFBJ4uq', 'dashboard,admins,users,activity', 1, '2023-11-07 14:08:19'),
+(2, 'andrew smith', 'andrew@tourister.in', '$2b$10$GvyBOIpUpcy5Ks2aTwa8hey3GJL1oA9hg09K6Oy0s.olYvEGNUFbq', 'dashboard,users,activity', 1, '2023-11-08 07:16:55'),
+(3, 'john doe', 'john@gmail.com', '$2b$10$3OhbuPvHovbtE4rBn/hEDONt3EKQKEwNOYTXKXjd1SKpYQc2D62LW', 'dashboard', 1, '2024-06-20 07:52:04'),
+(4, 'allen solly', 'allen@gmail.com', '$2b$10$L9v/3MdF5m7Y49DpuQOQvOLReWeM1jw5ktvL4mvUQyug8f97d1QYC', 'dashboard,activity', 1, '2024-06-21 11:31:38');
 
 -- --------------------------------------------------------
 
@@ -56,7 +85,7 @@ CREATE TABLE `email_verification` (
   `user_id` int(11) NOT NULL,
   `email` text NOT NULL,
   `entrydt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `email_verification`
@@ -83,19 +112,29 @@ CREATE TABLE `users` (
   `email` varchar(30) NOT NULL,
   `password` text NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
-  `entrydt` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `entrydt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deletedAt` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `dob`, `zipcode`, `email`, `password`, `status`, `entrydt`) VALUES
-(1, 'John Ngenga', '1992-10-05', '452010', 'john@gmail.com', '$2b$10$e7HmSD.nVZDShoQ2d70SeOcx97BoYwzchKcEtKuc8336ShLDqvugy', 1, '2023-11-07 12:00:37');
+INSERT INTO `users` (`id`, `name`, `dob`, `zipcode`, `email`, `password`, `status`, `entrydt`, `deletedAt`) VALUES
+(1, 'John Ngenga', '1992-11-26', '452011', 'john@gmail.com', '$2b$10$DM7X/0CBq8WF6mscHRu/PeWLiuAurhqaQdHdV6t1lgg6/zbFBJ4uq', 1, '2023-11-07 12:00:37', NULL),
+(2, 'albert istein', '2024-06-12', '457812', 'albert@gmail.com', '$2b$10$DM7X/0CBq8WF6mscHRu/PeWLiuAurhqaQdHdV6t1lgg6/zbFBJ4uq', 1, '2024-06-20 11:38:55', NULL),
+(3, 'sowel chempi', '2004-06-16', '452050', 'sowel@gmail.co', '$2b$10$DM7X/0CBq8WF6mscHRu/PeWLiuAurhqaQdHdV6t1lgg6/zbFBJ4uq', 0, '2024-06-20 11:39:02', '2024-06-21');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `activities_FK_1` (`adminId`);
 
 --
 -- Indexes for table `admins`
@@ -120,10 +159,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `email_verification`
@@ -135,7 +180,17 @@ ALTER TABLE `email_verification`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activities`
+--
+ALTER TABLE `activities`
+  ADD CONSTRAINT `activities_FK_1` FOREIGN KEY (`adminId`) REFERENCES `admins` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
